49fd21e74769abdcf8285c7bdf8ec4238b51a7af
ivenhov <iwan.daniel@gmail.com>
2017-04-03 23:56:09 +0100

    Added PermissionName

---
+++ src/main/java/com/ivenhov/permcheck/PermissionName.java
@@ -0,0 +1,13 @@
+package com.ivenhov.permcheck;
+
+/**
+*
+* @author daniel
+*/
+public enum PermissionName {
+
+    CreateSpace, DeleteSpace, UpdateSpace,
+    CreateVault, DeleteVault,
+    CreateUser, DeleteUser, UpdateUserInfo,
+    
+}
